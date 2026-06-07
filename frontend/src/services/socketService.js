cd7f60240a04c3d2c84d58b03dd23d52fcf72b24
roncd <roncd@github.com>
2026-06-07 23:36:18 +0200

    fix: utilisation export API_URL

--- frontend/src/services/socketService.js
+++ frontend/src/services/socketService.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
+import { API_URL } from '../config';
 
-const SOCKET_URL = 'http://localhost:3000';
+const SOCKET_URL = API_URL;;
 
 let socket = null;
 let currentRoom = null;
