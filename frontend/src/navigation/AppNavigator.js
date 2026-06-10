62159cd77d7af50e638061b8485377cf4d56109c
Mathis Lebel <162718920+YvetteMariee@users.noreply.github.com>
2026-06-10 21:48:03 +0200

    feat: ajouter docker-compose pour lancer frontend, backend et postgres

--- frontend/src/navigation/AppNavigator.js
+++ frontend/src/navigation/AppNavigator.js
@@ -13,7 +13,7 @@ import JoinLobbyController   from '../controllers/JoinLobbyController';
 import LobbyController    from '../controllers/LobbyController'; // TODO
 import ReadyController from '../controllers/ReadyController';
 import LyricsController from '../controllers/LyricsController';
-import VoteStarController from '../controllers/voteStarController'
+import VoteStarController from '../controllers/VoteStarController';
 
 const Stack = createNativeStackNavigator();
 
