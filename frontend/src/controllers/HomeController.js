47df853af755e76fe9a8c4f0d11d82081da39089
roncd <roncd@github.com>
2026-06-13 02:43:48 +0200

    feat: ajout clear session pour nouvel connexion

--- frontend/src/controllers/HomeController.js
+++ frontend/src/controllers/HomeController.js
@@ -5,11 +5,17 @@
 
 import React from 'react';
 import HomeView from '../views/HomeView';
+import { clearSession } from '../services/sessionService';
 
 export default function HomeController({ navigation }) {
-  const handleCreateLobby = () => navigation.navigate('CreateLobby');
-  const handleJoinLobby   = () => navigation.navigate('JoinLobby');
-
+  const handleCreateLobby = async () => {
+    await clearSession();
+    navigation.navigate('CreateLobby');
+  };
+  const handleJoinLobby = async () => {
+    await clearSession();
+    navigation.navigate('JoinLobby');
+  };
   return (
     <HomeView
       onCreateLobby={handleCreateLobby}
